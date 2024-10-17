c45ad1059bac8250717b11762b42970398d39c79
hunor64 <nhunor06@gmail.com>
2024-10-17 10:08:29 +0200

    css update

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
+import Resource from "./Components/Resource"
+
 const App = () => {
   return (
     <div className="container">
       <div className="PlayerName">
         <h1>PlayerName</h1>
       </div>
-      <div className="Resource">
-        <p>Steel: <span>50</span></p>
-        <div className="buttonContainer">
-
-          <button className="button">+</button>
-          <button className="button">-</button>
-        </div>
-      </div>
+      <Resource />
+      <Resource />
+      <Resource />
     </div>
   )
 }
---
+++ src/Components/Resource.tsx
@@ -0,0 +1,13 @@
+const Resource = () => {
+  return (
+    <div className="Resource">
+        <p>Steel: <span>50</span></p>
+        <div className="buttonContainer">
+          <button className="button">-</button>
+          <button className="button">+</button>
+        </div>
+      </div>
+  )
+}
+
+export default Resource
