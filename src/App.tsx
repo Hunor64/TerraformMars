d19e43acad7dc50a6c6d5085dbce14774bb2d890
hunor64 <nhunor06@gmail.com>
2024-10-17 11:37:58 +0200

    Code update

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,27 @@ import PlayerName from "./Components/PlayerName/PlayerName"
 import Resource from "./Components/Resource/Resource"
 
 const App = () => {
+    const MegaCredits = 0
+    const TermelesiSavok = 0
+    const Acel = 0
+    const Titan = 0
+    const Palantak = 0
+    const Energia = 0
+    const Ho = 0
 
-  return (
-    <div className="container">
-      <PlayerName/>
+    return (
+        <div className="container">
+            <PlayerName />
 
-      <Resource name="Megacredits" amount={0}/>
-      <Resource name="Termelési sávok" amount={0}/>
-      <Resource name="Acél" amount={0}/>
-      <Resource name="Titán" amount={0}/>
-      <Resource name="Palánták" amount={0}/>
-      <Resource name="Energia" amount={0}/>
-      <Resource name="Hő" amount={0}/>
-    </div>
-  )
+            <Resource name="Megacredits" amount={MegaCredits} />
+            <Resource name="Termelési sávok" amount={TermelesiSavok} />
+            <Resource name="Acél" amount={Acel} />
+            <Resource name="Titán" amount={Titan} />
+            <Resource name="Palánták" amount={Palantak} />
+            <Resource name="Energia" amount={Energia} />
+            <Resource name="Hő" amount={Ho} />
+        </div>
+    )
 }
 
 export default App
