60579510809b8366ad89fb4eccbe2f4a321667b3
hunor64 <nhunor06@gmail.com>
2024-10-17 09:48:51 +0200

    css added colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 const App = () => {
   return (
-    <>
-
-    </>
+    <div className="container">
+      <div className="PlayerName">
+        <h1>PlayerName</h1>
+      </div>
+      <div className="Resource">
+        <div className="buttonContainer">
+          <button className="button">+</button>
+          <button className="button">-</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
